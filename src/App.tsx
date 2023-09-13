8ee65396995955366a8918af4c94184765f60d0c
Thiago Henrique <thiagohcfidencio@gmail.com>
2023-09-13 17:10:17 -0300

    Finishing types

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,15 @@ import { zodResolver } from '@hookform/resolvers/zod';
 
 import './App.css';
 
+interface FormData {
+  firstName: string,
+  lastName: string,
+  email: string,
+  age: number,
+  password: string,
+  confirmPassword: string,
+}
+
 const App: FC = () => {
 
   const schema = z.object({
@@ -15,14 +24,14 @@ const App: FC = () => {
     password: z.string().min(4).max(25),
     confirmPassword: z.string().min(4).max(25),
   })
-    .refine(data => data.password === data.confirmPassword, {
-      message: 'Password do not match',
-      path: ['confirmPassword']
+    .refine((data) => data.password === data.confirmPassword, {
+      message: 'Passwords do not match',
+      path: ['confirmPassword'],
     });
 
-  const {register, handleSubmit} = useForm({resolver: zodResolver(schema) });
+  const {register, handleSubmit, formState: {errors}} = useForm<FormData>({resolver: zodResolver(schema) });
 
-  const onSubmit = (data) => {
+  const onSubmit = (data: FormData) => {
     console.log(data);
     
   };
@@ -33,26 +42,32 @@ const App: FC = () => {
         <label>
           First Name:
           <input type="text" {...register('firstName')} />
+          {errors.firstName && <span>{errors.firstName.message}</span>}
         </label>
         <label>
           Last Name:
           <input type="text" {...register('lastName')} />
+          {errors.lastName && <span>{errors.lastName.message}</span>}
         </label>
         <label>
           Email:
           <input type="email" {...register('email')} />
+          {errors.email && <span>{errors.email.message}</span>}
         </label>
         <label>
           Age:
           <input type="number" {...register('age', {valueAsNumber: true})} />
+          {errors.age && <span>{errors.age.message}</span>}
         </label>
         <label>
           Password:
           <input type="password" {...register('password')} />
+          {errors.password && <span>{errors.password.message}</span>}
         </label>
         <label>
           Confirm Password:
           <input type="password" {...register('confirmPassword')} />
+          {errors.confirmPassword && <span>{errors.confirmPassword.message}</span>}
         </label>
         <input type="submit" value="Submit" />
       </form>
